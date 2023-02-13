3e1697cc28adb9d61329093a2ff527b51fb0c349
Loise <loise.wambui@student.moringaschool.com>
2023-02-13 20:12:50 +0300

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
+
     
   end
 
+
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+puts "The weather outside is frightful.".sentence?
+puts "The weather outside is frightful.".question?
+puts "The weather outside is frightful.".exclamation?
+"This is a string! It has three sentences. Right?".count_sentences
